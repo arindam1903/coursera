9d28675213615a24db4267ace256e6d7e11da242
Arindam Dutta <arindam408@gmail.com>
2021-03-28 00:00:29 +0530

    update

---
+++ src/components/Home-container/HomeContainer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './HomeContainer.css';
+
+export default function HomeContainer(){
+
+    return(
+        <div id='main'>
+            <div id='container'>
+                <div id='title'>Your Course to Success</div>
+                <div id='description'>Build skills with courses, certificates, and degrees online from world-class universities and companies.
+
+
+</div>
+<button id='joinHome-button'>Join for Free</button>
+            </div>
+            <img src='https://d3njjcbhbojbot.cloudfront.net/api/utilities/v1/imageproxy/https://images.ctfassets.net/wp1lcwdav1p1/6JVJEaAT8FWwiBfVl1vECY/eccd28d76369194c1bae181bdef80140/hero-b.png?auto=format%2Ccompress&dpr=1&w=459&h=497&q=40'
+            alt='Home picture'/>
+        </div>
+    )
+}
